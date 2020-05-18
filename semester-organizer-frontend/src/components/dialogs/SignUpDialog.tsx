9c9c641f47fb1ed1688bef5200c7e039266b8159
Yadullah Duman <yadullah.duman@maibornwolff.de>
2020-05-18 02:38:18 +0200

    Extend eslint config for module resolution

--- semester-organizer-frontend/src/components/dialogs/SignUpDialog.tsx
+++ semester-organizer-frontend/src/components/dialogs/SignUpDialog.tsx
@@ -1,4 +1,6 @@
 import React, { useState, Fragment } from "react";
+import { Formik } from "formik";
+import * as Yup from "yup";
 import {
   Button,
   Dialog,
@@ -8,145 +10,167 @@ import {
   DialogActions,
 } from "@material-ui/core";
 
+import { SignUpFormValues } from "../../types/types";
+
 type SignUpDialogProps = {
   className: string;
 };
 
-export function SignUpDialog({ className }: SignUpDialogProps) {
+const initialValues: SignUpFormValues = {
+  firstname: "",
+  lastname: "",
+  username: "",
+  password: "",
+  confirmPassword: "",
+};
+
+const validationSchema = Yup.object({
+  firstname: Yup.string()
+    .required("First name is required")
+    .min(1, "Needs more than 1 character")
+    .max(30, "Must be 30 characters or less"),
+  lastname: Yup.string()
+    .required("Last name is required")
+    .min(1, "Needs more than 1 character")
+    .max(30, "Must be 30 characters or less"),
+  username: Yup.string()
+    .required("Username is required")
+    .min(4, "Needs more than 4 characters")
+    .max(20, "Must be 20 characters or less"),
+  password: Yup.string()
+    .required("Password is required")
+    .min(8, "Needs more than 8 characters")
+    .max(20, "Must be 30 characters or less")
+    .matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, "Password is too weak"),
+  confirmPassword: Yup.string()
+    .required("Confirm needs to be confirmed")
+    .oneOf([Yup.ref("password")], "Password does not match"),
+});
+
+export function SignUpDialog(props: SignUpDialogProps) {
   const [isOpen, setOpen] = useState(false);
-  const [firstname, setFirstname] = useState("");
-  const [lastname, setLastname] = useState("");
-  const [username, setUsername] = useState("");
-  const [password1, setPassword1] = useState("");
-  const [password2, setPassword2] = useState("");
 
   function openDialog() {
     setOpen(true);
   }
 
   function closeDialog() {
-    resetState();
     setOpen(false);
   }
 
-  function resetState() {
-    setFirstname("");
-    setLastname("");
-    setUsername("");
-    setPassword1("");
-    setPassword2("");
-  }
-
-  function handleFirstnameChange(event: any) {
-    setFirstname(event.target.value);
-  }
-
-  function handleLastnameChange(event: any) {
-    setLastname(event.target.value);
-  }
-
-  function handleUsernameChange(event: any) {
-    setUsername(event.target.value);
-  }
-
-  function handlePassword1Change(event: any) {
-    setPassword1(event.target.value);
-  }
-
-  function handlePassword2Change(event: any) {
-    setPassword2(event.target.value);
-  }
-
-  function handleSubmit() {
-    console.log(`
-      firstname: ${firstname}
-      lastname: ${lastname}
-      username: ${username}
-      password1: ${password1}
-      passowrd2 ${password2}
-    `);
+  function submit(values: SignUpFormValues) {
+    console.log(values);
   }
 
   return (
     <Fragment>
-      <Button variant="contained" color="primary" className={className} onClick={openDialog}>
-        Sign Up
-      </Button>
-      <Dialog
-        fullWidth
-        maxWidth="xs"
-        open={isOpen}
-        onClose={closeDialog}
-        aria-labelledby="signup-dialog-title"
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={(values: SignUpFormValues) => submit(values)}
       >
-        <DialogTitle id="signup-dialog-title">Sign Up</DialogTitle>
-        <DialogContent>
-          <TextField
-            required
-            autoFocus
-            fullWidth
-            value={firstname}
-            onChange={handleFirstnameChange}
-            id="signup-firstname"
-            label="First Name"
-            type="text"
-            margin="dense"
-            autoComplete="off"
-          />
-          <TextField
-            required
-            fullWidth
-            value={lastname}
-            onChange={handleLastnameChange}
-            id="signup-lastname"
-            label="Last Name"
-            type="text"
-            margin="dense"
-            autoComplete="off"
-          />
-          <TextField
-            required
-            fullWidth
-            value={username}
-            onChange={handleUsernameChange}
-            id="signup-username"
-            label="Username"
-            type="text"
-            margin="dense"
-            autoComplete="off"
-          />
-          <TextField
-            required
-            fullWidth
-            value={password1}
-            onChange={handlePassword1Change}
-            id="signup-password1"
-            label="Password"
-            type="password"
-            margin="dense"
-            autoComplete="off"
-          />
-          <TextField
-            required
-            fullWidth
-            value={password2}
-            onChange={handlePassword2Change}
-            id="signup-password2"
-            label="Verify Password"
-            type="password"
-            margin="dense"
-            autoComplete="off"
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={closeDialog} color="secondary">
-            Cancel
-          </Button>
-          <Button onClick={handleSubmit} color="primary">
-            Sign Up
-          </Button>
-        </DialogActions>
-      </Dialog>
+        {(formikProps) => {
+          const { values, errors, handleSubmit, handleChange, isValid } = formikProps;
+          return (
+            <Fragment>
+              <Button
+                variant="contained"
+                color="primary"
+                className={props.className}
+                onClick={openDialog}
+              >
+                Sign Up
+              </Button>
+              <Dialog
+                fullWidth
+                maxWidth="sm"
+                open={isOpen}
+                onClose={closeDialog}
+                aria-labelledby="signup-dialog-title"
+              >
+                <DialogTitle id="signup-dialog-title">Sign Up</DialogTitle>
+                <DialogContent>
+                  <form onSubmit={handleSubmit}>
+                    <TextField
+                      required
+                      helperText={errors.firstname}
+                      error={Boolean(errors.firstname)}
+                      value={values.firstname}
+                      onChange={handleChange}
+                      name="firstname"
+                      label="First Name"
+                      type="text"
+                      margin="dense"
+                      autoComplete="off"
+                      fullWidth
+                    />
+                    <TextField
+                      required
+                      helperText={errors.lastname}
+                      error={Boolean(errors.lastname)}
+                      value={values.lastname}
+                      onChange={handleChange}
+                      name="lastname"
+                      label="Last Name"
+                      type="text"
+                      margin="dense"
+                      autoComplete="off"
+                      fullWidth
+                    />
+                    <TextField
+                      required
+                      helperText={errors.username}
+                      error={Boolean(errors.username)}
+                      value={values.username}
+                      onChange={handleChange}
+                      name="username"
+                      label="Username"
+                      type="text"
+                      margin="dense"
+                      autoComplete="off"
+                      fullWidth
+                    />
+                    <TextField
+                      required
+                      helperText={errors.password}
+                      error={Boolean(errors.password)}
+                      value={values.password}
+                      onChange={handleChange}
+                      name="password"
+                      label="Password"
+                      type="password"
+                      margin="dense"
+                      autoComplete="off"
+                      fullWidth
+                    />
+                    <TextField
+                      required
+                      helperText={errors.confirmPassword}
+                      error={Boolean(errors.confirmPassword)}
+                      value={values.confirmPassword}
+                      onChange={handleChange}
+                      name="confirmPassword"
+                      label="Confirm Password"
+                      type="password"
+                      margin="dense"
+                      autoComplete="off"
+                      fullWidth
+                    />
+                  </form>
+                  <DialogActions>
+                    <Button onClick={closeDialog} color="secondary">
+                      Cancel
+                    </Button>
+                    <Button type="submit" color="primary" disabled={!isValid}>
+                      Submit
+                    </Button>
+                  </DialogActions>
+                </DialogContent>
+              </Dialog>
+            </Fragment>
+          );
+        }}
+      </Formik>
     </Fragment>
   );
 }
---
+++ semester-organizer-frontend/src/types/types.ts
@@ -0,0 +1,7 @@
+export type SignUpFormValues = {
+  firstname: string;
+  lastname: string;
+  username: string;
+  password: string;
+  confirmPassword: string;
+};
