abd5b29596c7af4fec843975179495fafb2cf356
Yadullah Duman <yadullah.duman@maibornwolff.de>
2020-05-25 22:59:32 +0200

    Populate teams page and call backend for data

--- src/components/pages/TeamsPage.tsx
+++ src/components/pages/TeamsPage.tsx
@@ -1,5 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import GroupIcon from "@material-ui/icons/Group";
+import { makeStyles, Typography, Button } from "@material-ui/core";
+
+import HeaderBar from "../common/HeaderBar";
+import { FlexContainer } from "../../utils/styled/styled-components";
+import { httpGet } from "../../utils/http-client";
+
+type Team = {
+  id: number;
+  name: string;
+};
+
+const useStyles = makeStyles(() => ({
+  button: {
+    margin: "8px",
+  },
+  error: {
+    margin: "8px",
+  },
+}));
 
 export default function TeamsPage() {
-  return <h3>Teams</h3>;
+  const [teams, setTeams] = useState([]);
+  const [hasError, setError] = useState(false);
+  const classes = useStyles();
+
+  async function fetchTeams() {
+    const response = await httpGet("/api/teams", true);
+    response
+      .json()
+      .then((res) => setTeams(res))
+      .catch((err) => setError(err));
+  }
+
+  useEffect(() => {
+    document.title = "Your Teams | FRA UAS Semester Organizer";
+    fetchTeams();
+  }, []);
+
+  return (
+    <div>
+      <HeaderBar title="Your Teams" />
+      <FlexContainer>
+        {teams.map((team: Team, index) => (
+          <Button
+            key={index}
+            variant="contained"
+            color="secondary"
+            size="large"
+            className={classes.button}
+            startIcon={<GroupIcon fontSize="large" />}
+          >
+            {team.name}
+          </Button>
+        ))}
+      </FlexContainer>
+      {hasError ? (
+        <Typography variant="body1" color="error" className={classes.error}>
+          Couldn&apos;t fetch your teams.
+        </Typography>
+      ) : null}
+    </div>
+  );
 }
--- src/utils/http-client.ts
+++ src/utils/http-client.ts
@@ -1,10 +1,32 @@
-export async function httpPost(url = "", data = {}) {
+import { getToken } from "./jwt";
+
+const headersDefault = {
+  "Content-Type": "application/json",
+};
+
+const headersWithAuth = {
+  "Content-Type": "application/json",
+  Authorization: "Bearer " + getToken(),
+};
+
+function getHeaders(isGuarded: boolean) {
+  return isGuarded ? headersWithAuth : headersDefault;
+}
+
+export async function httpPost(url = "", data = {}, isGuarded = false) {
   const response = await fetch(url, {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
+    headers: getHeaders(isGuarded),
     body: JSON.stringify(data),
+  });
+
+  return response;
+}
+
+export async function httpGet(url = "", isGuarded = false) {
+  const response = await fetch(url, {
+    method: "GET",
+    headers: getHeaders(isGuarded),
   });
 
   return response;
